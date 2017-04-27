eec1391e5c580d4b00ccd33879b46f0674968ecd
Tech Luminaire <info@techluminaire.co.uk>
2017-04-27 18:40:36 +0100

    first commit

---
+++ about.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+
+Get Your Sh*t Together 2017 
---
+++ assets/countdown.js
@@ -0,0 +1,35 @@
+CountDownTimer('05/22/2017 00:00 AM', 'countdown');
+
+function CountDownTimer(date, id)
+{
+    var end = new Date(date);
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById(id).innerHTML = 'EXPIRED!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        var html = [days, 'DAYS', hours, 'HOURS', minutes, 'MINUTES', seconds, 'SECONDS'].join(" ");
+
+        document.getElementById(id).innerHTML = html;
+    }
+
+    showRemaining();
+    timer = setInterval(showRemaining, 1000);
+}
